6c690d5c5fe7885601c9e8a4d72e6f607efdc39d
Justin <justin.crippen@gmail.com>
2021-12-06 18:12:49 -0600

    setting up views

--- uplandrapi/models/_init_.py
+++ uplandrapi/models/_init_.py
@@ -5,6 +5,7 @@ from gear import Gear
 from journal_entry import JournalEntry
 from location import Location
 from species_location import SpeciesLocation
+from species import Species
 from story import Story
 from weather import Weather
 
