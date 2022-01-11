4f0cb3323e2e728264161ec80ec08949b68b1145
Justin <justin.crippen@gmail.com>
2022-01-10 21:14:04 -0600

    Deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uplandr.wsgi --log-file -
--- uplandr/settings.py
+++ uplandr/settings.py
@@ -1,3 +1,4 @@
+import django_on_heroku
 import os 
 """
 Django settings for uplandr project.
@@ -143,3 +144,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
